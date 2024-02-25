2d3261ffd3d67d18d3ca9332f060be78d2a5b4fe
Arman Karshenas <46481124+armankarshenas@users.noreply.github.com>
2024-02-25 12:04:22 -0800

    Update EvaluateModel.m

--- Scripts/EvaluateModel.m
+++ Scripts/EvaluateModel.m
@@ -1,4 +1,9 @@
 function [] = EvaluateModel(net,Path_to_data)
+% EvaluateModel classifies sequences using a pre-trained network 
+% and computes the accuracy. 
+
+% Written by A. Karshenas -- Feb 25, 2024
+%----------------------------------------------------
     imds_test = imageDatastore(Path_to_data,'IncludeSubfolders',true,'LabelSource','foldernames');
     Y = Classify(net,imds_test.Labels);
     acc = nnz(Y==imds_test.Labels)/length(Y);
