87b1fbe28b0b9c1335bbbd6f41d945aaaef8f1af
Arman Karshenas <46481124+armankarshenas@users.noreply.github.com>
2024-02-25 12:06:34 -0800

    Update FindThreshold.m

--- Scripts/FindThreshold.m
+++ Scripts/FindThreshold.m
@@ -1,4 +1,8 @@
 function [th_up,th_down] = FindThreshold(tb_gene,tb_col,act_thresh)
+% FindThreshold finds the threshold for binning RNA counts 
+
+% Written by A. Karshenas -- Feb 25, 2024
+%----------------------------------------------------
     tb_col_name = string(tb_gene.Properties.VariableNames);
     tb_col_name = tb_col_name == tb_col;
     vec_values = table2array(tb_gene(:,tb_col_name));
